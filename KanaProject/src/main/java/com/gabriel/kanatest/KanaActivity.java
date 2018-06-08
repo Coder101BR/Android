629aa8fbb55263d6cc442314995815149bccbea2
Coder101BR <Coder101BR@users.noreply.github.com>
2018-06-07 22:45:31 -0300

    Added extra message and methods
    Added this for Hiragana and Katakana buttons

--- KanaProject/src/main/java/com/gabriel/kanatest/KanaActivity.java
+++ KanaProject/src/main/java/com/gabriel/kanatest/KanaActivity.java
@@ -1,7 +1,9 @@
 package com.gabriel.kanatest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class KanaActivity extends AppCompatActivity {
 
@@ -9,5 +11,29 @@ public class KanaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kana);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+
+
+        if(message.equals("Hiragana"))
+        {
+            textView.setText("Hiragana");
+        }
+        else if(message.equals("Katakana"))
+        {
+            textView.setText("Katakana");
+        }
+        else
+        {
+            textView.setText("Error");
+        }
+
     }
+
+
+
 }
--- KanaProject/src/main/java/com/gabriel/kanatest/MainActivity.java
+++ KanaProject/src/main/java/com/gabriel/kanatest/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.example.KanaTest.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,15 +14,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /** Called when the user taps the Send button */
+    /** Called when the user taps the Hiragana button */
     public void startHiragana(View view) {
         Intent intent = new Intent(this, KanaActivity.class);
-       // EditText editText = (EditText) findViewById(R.id.editText);
-       // String message = editText.getText().toString();
-       // intent.putExtra(EXTRA_MESSAGE, message);
+
+        String KanaSelect = "Hiragana";
+        intent.putExtra(EXTRA_MESSAGE, KanaSelect);
         startActivity(intent);
+    }
 
+    /** Called when the user taps the Katakana button */
+    public void startKatakana(View view) {
+        Intent intent = new Intent(this, KanaActivity.class);
 
+        String KanaSelect = "Katakana";
+        intent.putExtra(EXTRA_MESSAGE, KanaSelect);
+        startActivity(intent);
     }
 
 
