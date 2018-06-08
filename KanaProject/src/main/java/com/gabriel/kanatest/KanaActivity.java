8c9856fba3c25bd65fbcd7e61ae4b52e001c938b
Coder101BR <Coder101BR@users.noreply.github.com>
2018-06-08 13:58:22 -0300

    Added new method

--- KanaProject/src/main/java/com/gabriel/kanatest/KanaActivity.java
+++ KanaProject/src/main/java/com/gabriel/kanatest/KanaActivity.java
@@ -1,18 +1,45 @@
 package com.gabriel.kanatest;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.util.Random;
 
 public class KanaActivity extends AppCompatActivity {
 
     private RadioGroup radioGroup;
     private RadioButton OptionA, OptionB, OptionC, OptionD;
     private TextView textView;
+    Random rand = new Random();
+    Handler handler = new Handler();
+    long delay = 5000; // tempo de delay em millisegundos
+
+
+    public static String[][] novoKanaArray(int Escolhido, String[][] KanaArray)
+    {
+        String[][] NovoKanaArray = new String [KanaArray.length-1][KanaArray.length-1];
+        int i = 0;
+        int j = 0;
+
+        for(i = 0; i < KanaArray.length; i++){
+            if(i != Escolhido){
+                NovoKanaArray[j][0] = KanaArray[i][0];
+                NovoKanaArray[j][1] = KanaArray[i][1];
+                j++;
+            }
+        }
+
+        return NovoKanaArray;
+    }
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +71,12 @@ public class KanaActivity extends AppCompatActivity {
 
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                
+                //radioGroup.clearCheck();
                 // find which radio button is selected
                 if(checkedId == R.id.OptionA)
                 {
-
                       Toast.makeText(getApplicationContext(), "OptionA",Toast.LENGTH_SHORT).show();
+
                 }
                 else if(checkedId == R.id.OptionB)
                 {
@@ -64,15 +91,31 @@ public class KanaActivity extends AppCompatActivity {
                      Toast.makeText(getApplicationContext(), "OptionD", Toast.LENGTH_SHORT).show();
                 }
 
-                //radioGroup.clearCheck();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        // código a ser executado após o tempo de delay
+                        radioGroup.clearCheck(); // // Radio button checked
+
+                    }
+                }, delay);
+
+
             }
 
         });
 
 
+        OptionA = (RadioButton) findViewById(R.id.OptionA);
+        OptionB = (RadioButton) findViewById(R.id.OptionB);
+        OptionC = (RadioButton) findViewById(R.id.OptionC);
+        OptionD = (RadioButton) findViewById(R.id.OptionD);
+        textView = (TextView) findViewById(R.id.text);
 
-
+        textView.setText("か");
     }
+
+
+
 
     String[][] KanaArray = new String [][] {
             { "a", "あ"},
@@ -133,6 +176,7 @@ public class KanaActivity extends AppCompatActivity {
             { "n", "ん"}
 
     };
+
 
 
 
