2dd56fb581ce39707c6b5cfc534745894bd2addf
Coder101BR <Coder101BR@users.noreply.github.com>
2018-06-08 11:57:45 -0300

    Updated Kana java
    Added string kana array and radiogroup

--- KanaProject/src/main/java/com/gabriel/kanatest/KanaActivity.java
+++ KanaProject/src/main/java/com/gabriel/kanatest/KanaActivity.java
@@ -3,9 +3,16 @@ package com.gabriel.kanatest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class KanaActivity extends AppCompatActivity {
+
+    private RadioGroup radioGroup;
+    private RadioButton OptionA, OptionB, OptionC, OptionD;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +38,102 @@ public class KanaActivity extends AppCompatActivity {
             textView.setText("Error");
         }
 
+        radioGroup = (RadioGroup) findViewById(R.id.myRadioGroup);
+        radioGroup.clearCheck();  // clean any radio button checked
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                
+                // find which radio button is selected
+                if(checkedId == R.id.OptionA)
+                {
+
+                      Toast.makeText(getApplicationContext(), "OptionA",Toast.LENGTH_SHORT).show();
+                }
+                else if(checkedId == R.id.OptionB)
+                {
+                     Toast.makeText(getApplicationContext(), "OptionB", Toast.LENGTH_SHORT).show();
+                }
+                else if(checkedId == R.id.OptionC)
+                {
+                     Toast.makeText(getApplicationContext(), "OptionC", Toast.LENGTH_SHORT).show();
+                }
+                else if(checkedId == R.id.OptionD)
+                {
+                     Toast.makeText(getApplicationContext(), "OptionD", Toast.LENGTH_SHORT).show();
+                }
+
+                //radioGroup.clearCheck();
+            }
+
+        });
+
+
+
+
     }
+
+    String[][] KanaArray = new String [][] {
+            { "a", "あ"},
+            { "i", "い"},
+            { "u", "う"},
+            { "e", "え"},
+            { "o", "お"},
+
+            { "ka", "か"},
+            { "ki", "き"},
+            { "ku", "く"},
+            { "ke", "け"},
+            { "ko", "こ"},
+
+            { "sa", "さ"},
+            { "shi", "し"},
+            { "su", "す"},
+            { "se", "せ"},
+            { "so", "そ"},
+
+            { "ta", "た"},
+            { "chi", "ち"},
+            { "tsu", "つ"},
+            { "te", "て"},
+            { "to", "と"},
+
+            { "na", "な"},
+            { "ni", "に"},
+            { "nu", "ぬ"},
+            { "ne", "ね"},
+            { "no", "の"},
+
+            { "ha", "は"},
+            { "hi", "ひ"},
+            { "fu", "ふ"},
+            { "he", "へ"},
+            { "ho", "ほ"},
+
+            { "ma", "ま"},
+            { "mi", "み"},
+            { "mu", "む"},
+            { "me", "め"},
+            { "mo", "も"},
+
+            { "ya", "や"},
+            { "yu", "ゆ"},
+            { "yo", "よ"},
+
+            { "ra", "ら"},
+            { "ri", "り"},
+            { "ru", "る"},
+            { "re", "れ"},
+            { "ro", "ろ"},
+
+            { "wa", "わ"},
+            { "wo", "を"},
+
+            { "n", "ん"}
+
+    };
+
 
 
 
